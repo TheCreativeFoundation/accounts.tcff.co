4c1c2321cc7241587e104491ce62ae9284339089
Armaan Gupta <33143849+guppythegod@users.noreply.github.com>
2018-08-15 10:43:37 -0500

    started server file

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ const express = require("express");
 
 const app = express();
 
+function verifytoken() {
+    
+}
+
+app.get("/createtoken", (req, res) => {
+    
+});
+
+
 app.listen(80, () => {
     console.log("Listening on port 80...");
 });
